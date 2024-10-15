1c46b7cfaeb0eff1bb178f075f4bf47de82dede0
spacemanspiff2007 <10754716+spacemanspiff2007@users.noreply.github.com>
2024-10-15 14:54:15 +0200

    Update to Python 3.13 (#13)

--- src/sphinx_exec_code/__init__.py
+++ src/sphinx_exec_code/__init__.py
@@ -1,5 +1,3 @@
-from .__version__ import __version__
-
-# isort: split
-
 from sphinx_exec_code.sphinx_api import setup
+
+from .__version__ import __version__
--- tests/test_code_exec.py
+++ tests/test_code_exec.py
@@ -1,4 +1,5 @@
 import os
+import sys
 from pathlib import Path
 
 import pytest
@@ -8,7 +9,7 @@ from sphinx_exec_code.configuration import PYTHONPATH_FOLDERS, SET_UTF8_ENCODING
 
 
 @pytest.fixture()
-def _setup_env(monkeypatch):
+def _setup_env(monkeypatch) -> None:
     f = Path(__file__).parent
     monkeypatch.setattr(WORKING_DIR, '_value', f)
     monkeypatch.setattr(PYTHONPATH_FOLDERS, '_value', [str(f)])
@@ -16,7 +17,7 @@ def _setup_env(monkeypatch):
 
 @pytest.mark.parametrize('utf8', [True, False])
 @pytest.mark.usefixtures('_setup_env')
-def test_print(monkeypatch, utf8):
+def test_print(monkeypatch, utf8) -> None:
     monkeypatch.setattr(SET_UTF8_ENCODING, '_value', utf8)
 
     code = "print('Line1')\nprint('Line2')"
@@ -26,7 +27,7 @@ def test_print(monkeypatch, utf8):
 
 @pytest.mark.parametrize('utf8', [True, False])
 @pytest.mark.usefixtures('_setup_env')
-def test_print_table(monkeypatch, utf8):
+def test_print_table(monkeypatch, utf8) -> None:
     monkeypatch.setattr(SET_UTF8_ENCODING, '_value', utf8)
 
     code = "\n \n  \n\n" \
@@ -36,9 +37,13 @@ def test_print_table(monkeypatch, utf8):
     assert output == '      | A | B |\n Col1 | 1 | 2 |'
 
 
+PYTHON_3_13 = sys.version_info[:2] >= (3, 13)
+
+
+@pytest.mark.skipif(PYTHON_3_13, reason='Old traceback')
 @pytest.mark.parametrize('utf8', [True, False])
 @pytest.mark.usefixtures('_setup_env')
-def test_err(monkeypatch, utf8):
+def test_err_12(monkeypatch, utf8) -> None:
     monkeypatch.setattr(SET_UTF8_ENCODING, '_value', utf8)
 
     code = "print('Line1')\nprint('Line2')\n1/0"
@@ -58,12 +63,38 @@ def test_err(monkeypatch, utf8):
     ]
 
 
+@pytest.mark.skipif(not PYTHON_3_13, reason='Old traceback')
+@pytest.mark.parametrize('utf8', [True, False])
+@pytest.mark.usefixtures('_setup_env')
+def test_err_13(monkeypatch, utf8) -> None:
+    monkeypatch.setattr(SET_UTF8_ENCODING, '_value', utf8)
+
+    code = "print('Line1')\nprint('Line2')\n1/0"
+
+    with pytest.raises(CodeExceptionError) as e:
+        execute_code(code, Path('/my_file'), 5)
+
+    msg = e.value.pformat()
+
+    assert msg == [
+        "   print('Line1')",
+        "   print('Line2')",
+        '   1/0 <--',
+        '',
+        'Traceback (most recent call last):',
+        '  File "my_file", line 7',
+        '    1/0',
+        '    ~^~',
+        'ZeroDivisionError: division by zero'
+    ]
+
+
 IS_WIN = os.name == 'nt'
 
 
 @pytest.mark.skipif(not IS_WIN, reason='Windows only')
 @pytest.mark.usefixtures('_setup_env')
-def test_unicode_fails(monkeypatch):
+def test_unicode_fails(monkeypatch) -> None:
     code = "print('●')"
 
     monkeypatch.setattr(SET_UTF8_ENCODING, '_value', False)
@@ -78,7 +109,7 @@ def test_unicode_fails(monkeypatch):
 
 @pytest.mark.skipif(IS_WIN, reason='Fails on Windows')
 @pytest.mark.usefixtures('_setup_env')
-def test_unicode_no_utf8(monkeypatch):
+def test_unicode_no_utf8(monkeypatch) -> None:
     code = "print('●')"
 
     monkeypatch.setattr(SET_UTF8_ENCODING, '_value', False)
